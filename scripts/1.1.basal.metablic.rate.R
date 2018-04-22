4a1ad99d86c5f787e9d474ddc1d8a65e0c3b671a
rasmus87 <rasmus.iwfly@gmail.com>
2018-04-22 13:10:00 +0200

    Update scripts 1.1 and 1.2 and rebuild bmr and fmr datasets

--- scripts/1.1.basal.metablic.rate.R
+++ scripts/1.1.basal.metablic.rate.R
@@ -4,13 +4,13 @@ library(tidyverse)
 
 mam <- read_csv("../PHYLACINE_1.1/Data/Traits/Trait_data.csv", col_types = cols())
 
-cap <- read_tsv("data/MR/Capellini2010.txt", col_types = cols(), na = "-9999")
+cap <- read_tsv("data/Capellini2010.txt", col_types = cols(), na = "-9999")
 names(cap) <- make.names(names(cap))
 cap <- cap %>% mutate(Species = str_replace(Species, "(.* \\()", ""))
 cap <- cap %>% mutate(Species = str_replace(Species, "\\)", ""))
 cap <- cap %>% mutate(Species = str_replace(Species, " ", "_"))
 # Load taxonomy solver
-cap.tax <- read_csv("data/MR/Capellini2010_tax.solver.csv", col_types = cols())
+cap.tax <- read_csv("data/Capellini2010_tax.solver.csv", col_types = cols())
 cap <- left_join(cap, cap.tax %>% select(Species, Binomial.1.2), by = "Species")
 cap <- cap %>% mutate(Binomial.1.2 = ifelse(is.na(Binomial.1.2), Species, Binomial.1.2))
 stopifnot(nrow(anti_join(cap, mam, by = "Binomial.1.2")) == 0)
@@ -22,7 +22,7 @@ cap <- cap %>% select(Binomial.1.2, Order.1.2, Family.1.2, log10BMR, log10BM) %>
   mutate(BMR.source = "Capellini.2010")
 
 
-pantheria <- read_tsv("data/MR/PanTHERIA_1-0_WR05_Aug2008.txt", col_types = cols())
+pantheria <- read_tsv("data/PanTHERIA_1-0_WR05_Aug2008.txt", col_types = cols())
 names(pantheria) <- make.names(names(pantheria))
 pantheria <- pantheria %>% 
   filter(!is.na(X18.1_BasalMetRate_mLO2hr)) %>% 
@@ -62,7 +62,7 @@ pantheria <- pantheria %>%
 # 4.69 kcal / L O2 # Assuming pure fat burn
 # 4.87 kcal / L O2 # Assuming mixed burn
 # 4184 J / kcal
-# 4184*c(5.05, 4.87, 4.69)
+# 4184 * c(5.05, 4.87, 4.69)
 
 bmr <- rbind(cap, pantheria)
 #http://www.jbc.org/content/59/1/41.full.pdf
@@ -77,32 +77,8 @@ bmr$log10BMR <- log10(bmr.kj.day)
 
 write_csv(bmr, "builds/bmr.csv")
 
-
-################################################################################
-
-# Look at out dataset
-
-hc <- readxl::read_xlsx("data/halls cave clean.xlsx", sheet = "hall cave clean")
-hc <- hc %>% mutate(Binomial.1.2 = paste(genus.new, 
-                                         ifelse(is.na(likely.sp), species.new, likely.sp),
-                                         sep = "_"))
-hc <- left_join(hc, mam %>% dplyr::select(Binomial.1.2:Family.1.2))
-anti_join(hc, bmr, by = "Order.1.2") %>% select(Order.1.2, Family.1.2, Binomial.1.2)
-
+# Display data:
 ggplot(bmr, aes(log10BM, log10BMR)) + 
   geom_point(aes(col = Order.1.2, shape = BMR.source)) +
   geom_smooth(method = "loess", col = "blue") +
   geom_smooth(method = "lm", col = "red")
-
-m <- lm(log10BMR ~ log10BM, data = bmr)
-hc.fmr <- hc %>% left_join(mam) %>% transmute(log10BM = log10(Mass.g),
-                                              log10BMR = predict(m, data.frame(log10BM)),
-                                              lwr = predict(m, data.frame(log10BM), interval = "predict")[, 2],
-                                              upr = predict(m, data.frame(log10BM), interval = "predict")[, 3])
-
-
-ggplot(bmr, aes(log10BM, log10BMR)) +
-  geom_ribbon(data = hc.fmr, aes(ymin = lwr, ymax = upr, group = NULL), alpha = 0.2) +
-  geom_point(data = hc.fmr, col = "red", pch = 3, stroke = 1.3) +
-  geom_line(data = hc.fmr, col = "red") +
-  geom_point(aes(col = BMR.source))
--- scripts/1.2.field.metablic.rate.R
+++ scripts/1.2.field.metablic.rate.R
@@ -5,7 +5,7 @@ library(stringr)
 
 mam <- read_csv("../PHYLACINE_1.1/Data/Traits/Trait_data.csv", col_types = cols())
 
-fmr <- read_csv("data/MR/jane12086-sup-0003-AppendixS5.csv", col_types = cols())
+fmr <- read_csv("data/jane12086-sup-0003-AppendixS5.csv", col_types = cols())
 names(fmr) <- make.names(names(fmr))
 fmr <- fmr %>%
   filter(Class == "Mammalia") %>% 
@@ -19,9 +19,9 @@ stopifnot(anti_join(fmr, mam, by = "Binomial.1.2") %>% nrow == 0)
 fmr <- left_join(fmr, mam %>% dplyr::select(Binomial.1.2:Family.1.2), by = "Binomial.1.2")
 fmr <- fmr %>% select(Binomial.1.2, Order.1.2, Family.1.2, log10FMR, log10BM, FMR.source)
 
-nagy <- read_csv("data/MR/Nagy1999.csv", col_types = cols())
+nagy <- read_csv("data/Nagy1999.csv", col_types = cols())
 names(nagy) <- make.names(names(nagy))
-tax.nagy <- read_csv("data/MR/Nagy1999_tax.solver.csv", col_types = cols())
+tax.nagy <- read_csv("data/Nagy1999_tax.solver.csv", col_types = cols())
 nagy <- left_join(nagy, tax.nagy, by = c("Species", "Common.name"))
 nagy <- nagy %>% mutate(Species = ifelse(is.na(Binomial.1.2), Species, Binomial.1.2))
 nagy <- nagy %>% mutate(Binomial.1.2 = str_replace(Species, " ", "_"))
@@ -36,13 +36,13 @@ nagy <- nagy %>% select(Binomial.1.2, Order.1.2, Family.1.2, log10FMR, log10BM) 
 fmr <- rbind(fmr, nagy)
 
 
-cap <- read_tsv("data/MR/Capellini2010.txt", col_types = cols(), na = "-9999")
+cap <- read_tsv("data/Capellini2010.txt", col_types = cols(), na = "-9999")
 names(cap) <- make.names(names(cap))
 cap <- cap %>% mutate(Species = str_replace(Species, "(.* \\()", ""))
 cap <- cap %>% mutate(Species = str_replace(Species, "\\)", ""))
 cap <- cap %>% mutate(Species = str_replace(Species, " ", "_"))
 # Load taxonomy solver
-cap.tax <- read_csv("data/MR/Capellini2010_tax.solver.csv", col_types = cols())
+cap.tax <- read_csv("data/Capellini2010_tax.solver.csv", col_types = cols())
 cap <- left_join(cap, cap.tax %>% select(Species, Binomial.1.2), by = "Species")
 cap <- cap %>% mutate(Binomial.1.2 = ifelse(is.na(Binomial.1.2), Species, Binomial.1.2))
 stopifnot(nrow(anti_join(cap, mam, by = "Binomial.1.2")) == 0)
@@ -56,7 +56,7 @@ cap <- cap %>% select(Binomial.1.2, Order.1.2, Family.1.2, log10FMR, log10BM) %>
 
 fmr <- rbind(fmr, cap)
 
-add <- read_csv("data/MR/additions.csv", col_types = cols())
+add <- read_csv("data/additions.csv", col_types = cols())
 add <- add %>% mutate(log10BM = log10(mass.g), log10FMR = log10(fmr.kJ.day)) %>% 
   left_join(mam, by = "Binomial.1.2") %>% 
   select(Binomial.1.2, Order.1.2, Family.1.2, log10FMR, log10BM, FMR.source)
@@ -74,29 +74,6 @@ ggplot(fmr, aes(log10BM, log10FMR)) +
                  col = Family.1.2)) +
   geom_smooth(method = "loess", col = "black") +
   geom_smooth(method = "lm", col = "black", lty = "dashed")
-
-# Model and calculate prediction interval
-m <- lm(log10FMR ~ log10BM, data = fmr)
-hc.fmr <- hc %>% left_join(mam) %>% transmute(log10BM = log10(Mass.g),
-                                              log10FMR = predict(m, data.frame(log10BM)),
-                                              lwr = predict(m, data.frame(log10BM), interval = "predict")[, 2],
-                                              upr = predict(m, data.frame(log10BM), interval = "predict")[, 3])
-
-# Merge with Halls Cave data to look at how well they are represented
-hc <- readxl::read_xlsx("data/halls cave clean.xlsx", sheet = "hall cave clean")
-hc <- hc %>% mutate(Binomial.1.2 = paste(genus.new, 
-                                         ifelse(is.na(likely.sp), species.new, likely.sp),
-                                         sep = "_"))
-hc <- left_join(hc, mam %>% dplyr::select(Binomial.1.2:Family.1.2), col_types = cols())
-anti_join(hc, fmr, by = "Order.1.2") %>% select(Order.1.2, Family.1.2, Binomial.1.2)
-
-ggplot(fmr, aes(log10BM, log10FMR, col = FMR.source)) +
-  geom_ribbon(data = hc.fmr, aes(col = NULL, shape = NULL, ymin = lwr, ymax = upr, group = NULL), alpha = 0.2) +
-  geom_point(data = hc.fmr, aes(col = NULL, shape = NULL), col = "red", pch = 3, stroke = 1.3) +
-  geom_line(data = hc.fmr, aes(col = NULL, shape = NULL), col = "red") +
-  geom_point()
-
-
 
 # Not run: Exclude some things
 # fmr <-  fmr %>%
