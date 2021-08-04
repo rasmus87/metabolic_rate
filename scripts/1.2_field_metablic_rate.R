1c83b53241d8dadfcfac1c5150c821ba56131546
rasmus87 <rasmus.iwfly@gmail.com>
2021-08-04 20:36:51 +0200

    Clean field metabolic rate script

--- scripts/1.2_field_metablic_rate.R
+++ scripts/1.2_field_metablic_rate.R
@@ -1,66 +1,132 @@
-# Compile FMR: kJ/day
+# Compile FMR dataset from various sources
+# 30/07-2021 Rasmus Ø Pedersen
 
+# Load libraries
 library(tidyverse)
-library(stringr)
+library(ggrepel)
+library(ggpmisc)
 
+# Load PHYLACINE 1.2.1
 mam <- read_csv("../PHYLACINE_1.2/Data/Traits/Trait_data.csv", col_types = cols())
 
-## Load Hudson.2013
-hudson <- read_csv("data/jane12086-sup-0003-AppendixS5.csv", col_types = cols())
-names(hudson) <- make.names(names(hudson))
+
+
+# Load Hudson.2013 --------------------------------------------------------
+
+# Load data
+hudson <- read_csv("data/jane12086-sup-0003-AppendixS5.csv")
 hudson <- hudson %>%
   filter(Class == "Mammalia") %>% 
-  mutate(Binomial.Source = paste(Genus, Species, sep = "_")) %>% 
-  mutate(BM = M..kg.*1000, log10BM = log10(M..kg.*1000), 
-         FMR = FMR..kJ...day., log10FMR = log10(FMR..kJ...day.)) %>% 
-  mutate(Binomial.1.2 = str_replace(Binomial.Source, "Lama_glama", "Lama_guanicoe")) %>% 
-  mutate(FMR.source = "Hudson.2013")
-stopifnot(anti_join(hudson, mam, by = "Binomial.1.2") %>% nrow == 0)
+  mutate(Binomial.Source = paste(Genus, Species, sep = "_"),
+         BM = `M (kg)` * 1000, 
+         FMR = `FMR (kJ / day)`, 
+         log10BM = log10(BM), 
+         log10FMR = log10(FMR),
+         Binomial.1.2 = str_replace(Binomial.Source, "Lama_glama", "Lama_guanicoe"),
+         Source = "Hudson.2013")
+# Check that all are in there
+stopifnot(all(hudson$Binomial.1.2 %in% mam$Binomial.1.2))
 
-hudson <- left_join(hudson, mam %>% dplyr::select(Binomial.1.2:Family.1.2), by = "Binomial.1.2")
-hudson <- hudson %>% transmute(Binomial.1.2, Order.1.2, Family.1.2, Binomial.Source, BM, FMR, log10BM, log10FMR, Source = FMR.source)
+# Add Family and Order from PHYLACINE
+hudson <- left_join(hudson, mam, by = "Binomial.1.2")
+# Make tibble names uniform
+hudson <- hudson %>% transmute(Binomial.1.2,
+                               Order.1.2,
+                               Family.1.2, 
+                               Binomial.Source, 
+                               BM, 
+                               FMR, 
+                               log10BM, 
+                               log10FMR, 
+                               Source)
 
-## Load Hudson.1999
-nagy <- read_csv("data/Nagy1999.csv", col_types = cols())
-names(nagy) <- make.names(names(nagy))
-tax.nagy <- read_csv("data/Nagy1999_tax.solver.csv", col_types = cols())
+
+# Load Nagy.1999 --------------------------------------------------------
+
+# Load data
+nagy <- read_csv("data/Nagy1999.csv")
+
+# Load Nagy 1999 tax solver
+tax.nagy <- read_csv("data/Nagy1999_tax.solver.csv")
 nagy <- left_join(nagy, tax.nagy, by = c("Species", "Common.name"))
-nagy <- nagy %>% mutate(Binomial.Source = Species, Species = ifelse(is.na(Binomial.1.2), Species, Binomial.1.2))
-nagy <- nagy %>% mutate(Binomial.1.2 = str_replace(Species, " ", "_"))
-stopifnot(anti_join(nagy, mam, by = "Binomial.1.2") %>% nrow == 0)
+nagy <- nagy %>% mutate(Binomial.Source = Species, 
+                        Species = coalesce(Binomial.1.2, Species),
+                        Binomial.1.2 = str_replace(Species, " ", "_"))
+# Check that all are in there
+stopifnot(all(nagy$Binomial.1.2 %in% mam$Binomial.1.2))
 
-nagy <- left_join(nagy, mam %>% dplyr::select(Binomial.1.2:Family.1.2), by = "Binomial.1.2")
-nagy <- nagy %>% mutate(BM = Mass..g., log10BM = log10(Mass..g.), FMR = FMR..kJ.day., log10FMR = log10(FMR..kJ.day.))
-nagy <- nagy %>% transmute(Binomial.1.2, Order.1.2, Family.1.2, Binomial.Source, BM, FMR, log10BM, log10FMR, Source = "Nagy.1999")
+# Add Family and Order from PHYLACINE
+nagy <- left_join(nagy, mam, by = "Binomial.1.2")
+# Make tibble names uniform
+nagy <- nagy %>% transmute(Binomial.1.2,
+                           Order.1.2, 
+                           Family.1.2,
+                           Binomial.Source,
+                           BM = `Mass.(g)`, 
+                           FMR = `FMR.(kJ/day)`, 
+                           log10BM = log10(BM),
+                           log10FMR = log10(FMR),
+                           Source = "Nagy.1999")
 
-## Load Capellini.2010
+# Load Capellini.2010 --------------------------------------------------------
+
+# Load data
 cap <- read_tsv("data/Capellini2010.txt", col_types = cols(), na = "-9999")
-names(cap) <- make.names(names(cap))
-cap <- cap %>% mutate(Species = str_replace(Species, "(.* \\()", ""))
-cap <- cap %>% mutate(Species = str_replace(Species, "\\)", ""))
-cap <- cap %>% mutate(Species = str_replace(Species, " ", "_"))
-# Load taxonomy solver
+# Use names in paranthesis where available and replace spaces with underscore as PHYLACINE
+cap <- cap %>% mutate(Species = str_replace(Species, "(.* \\()", ""),
+                      Species = str_replace(Species, "\\)", ""),
+                      Species = str_replace(Species, " ", "_"))
+
+# Load Capellini 2010 taxonomy solver
 cap.tax <- read_csv("data/Capellini2010_tax.solver.csv", col_types = cols())
 cap <- left_join(cap, cap.tax %>% select(Species, Binomial.1.2), by = "Species")
-cap <- cap %>% mutate(Binomial.Source = Species, Binomial.1.2 = ifelse(is.na(Binomial.1.2), Species, Binomial.1.2))
-stopifnot(nrow(anti_join(cap, mam, by = "Binomial.1.2")) == 0)
+# Update names where appropriate
+cap <- cap %>% mutate(Binomial.1.2 = coalesce(Binomial.1.2, Species))
+# Check that we got them all
+stopifnot(all(cap$Binomial.1.2 %in% mam$Binomial.1.2))
 
-cap <- left_join(cap, mam %>% dplyr::select(Binomial.1.2:Family.1.2), by = "Binomial.1.2")
-cap <- cap %>% mutate(BM = Body.mass.for.FMR..gr., log10BM = log10(Body.mass.for.FMR..gr.), FMR = FMR..kJ.day., log10FMR = log10(FMR..kJ.day.))
-cap <- cap %>% filter(!is.na(log10FMR))
-cap <- cap %>% transmute(Binomial.1.2, Order.1.2, Family.1.2, Binomial.Source, BM, FMR, log10BM, log10FMR, Source = "Capellini.2010")
+# Add Family and Order from PHYLACINE
+cap <- left_join(cap, mam, by = "Binomial.1.2")
+# Make tibble names uniform and remove data with missing FMR
+cap <- cap %>% 
+  transmute(Binomial.1.2, 
+            Order.1.2, 
+            Family.1.2,
+            Binomial.Source = Species,
+            BM = `Body mass for FMR (gr)`, 
+            FMR = `FMR (kJ/day)`, 
+            log10BM = log10(BM), 
+            log10FMR = log10(FMR),
+            Source = "Capellini.2010") %>% 
+  filter(!is.na(log10FMR))
 
-## Load additions
+# Add extra data from smaller sources -------------------------------------
+
+# Load additions
 add <- read_csv("data/additions.fmr.csv", col_types = cols())
+# Transform additions so they align
 add <- add %>% mutate(BM = mass.g,
-                      log10BM = log10(mass.g),
                       FMR = fmr.kJ.day,
-                      log10FMR = log10(fmr.kJ.day)) %>% 
+                      log10BM = log10(BM),
+                      log10FMR = log10(FMR)) %>% 
   left_join(mam, by = "Binomial.1.2") %>% 
-  transmute(Binomial.1.2, Order.1.2, Family.1.2, Binomial.Source = Binomial.1.2, BM, FMR, log10BM, log10FMR, Source = FMR.source, Comment = FMR.comment)
+  transmute(Binomial.1.2, 
+            Order.1.2, 
+            Family.1.2,
+            Binomial.Source = Binomial.1.2, 
+            BM,
+            FMR, 
+            log10BM, 
+            log10FMR, 
+            Source = FMR.source, 
+            Comment = FMR.comment)
 
+# Combine datasets --------------------------------------------------------
+
+# Combine
 fmr <- bind_rows(hudson, cap, nagy, add)
 
+# Non terrestrials and humans
 bat.order <- "Chiroptera"
 sea.cow.order <- "Sirenia"
 whale.families <- c("Balaenidae", "Balaenopteridae", "Ziphiidae", 
@@ -69,14 +135,30 @@ whale.families <- c("Balaenidae", "Balaenopteridae", "Ziphiidae",
                     "Phocoenidae", "Platanistidae")
 seal.families <- c("Otariidae", "Phocidae", "Odobenidae")
 marine.carnivores <- c("Enhydra_lutris", "Lontra_felina", "Ursus_maritimus")
+humans <- "Homo"
 
-terrestrial <- mam %>% filter(!Order.1.2 %in% c(bat.order, sea.cow.order),
-                              !Family.1.2 %in% c(whale.families, seal.families),
-                              !Binomial.1.2 %in% marine.carnivores) %>% pull(Binomial.1.2)
+# Terrestrial species list:
+terrestrial <- mam %>% 
+  filter(!Order.1.2 %in% c(bat.order, sea.cow.order),
+         !Family.1.2 %in% c(whale.families, seal.families),
+         !Binomial.1.2 %in% marine.carnivores,
+         !Genus.1.2 %in% humans) %>% 
+  pull(Binomial.1.2)
+
+# Filter BMR dataset to the terrestrial list
 fmr <- fmr %>% filter(Binomial.1.2 %in% terrestrial)
 
-d <- which(duplicated(fmr[c("Binomial.1.2", "log10BM", "log10FMR")]))
-fmr <- fmr[-d, ]
+# Check for exact duplicated values and remove
+fmr <- fmr %>% 
+  distinct(Binomial.1.2, log10BM, log10FMR, .keep_all = TRUE)
+
+# Visually inspect data
+ggplot(fmr, aes(log10BM, log10FMR)) +
+  geom_point() + 
+  geom_smooth(method = lm) +
+  stat_dens2d_filter(aes(label = paste(Source, Binomial.1.2, sep = ": ")),
+                     geom = "text_repel", keep.fraction = 0.005)
+
 
 # Check for pseudo duplicated values
 # I.e. values that we think within data certainty might be duplicated
@@ -95,10 +177,10 @@ for(species in d) {
   if(length(duplicates) > 0) fmr <- fmr[-duplicates, ]
 }
 
-write_csv(fmr, "builds/fmr2.csv")
+# Write data out and check visually that it seems OK ----------------------
 
-
-################################################################################
+# Write data
+write_csv(fmr, "builds/fmr_data.csv")
 
 # Display data regarding data source:
 ggplot(fmr, aes(log10BM, log10FMR)) + 
