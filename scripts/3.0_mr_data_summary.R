c88713f60b36e8d7da03a3f8cac80491eddda515
rasmus87 <rasmus.iwfly@gmail.com>
2021-12-08 16:01:23 +0100

    Move MR data summary

--- scripts/3.0_mr_data_summary.R
+++ scripts/3.0_mr_data_summary.R
@@ -1,19 +1,27 @@
+# Run imputation for all trees
+
+# Load libraries
 library(tidyverse)
 
 # Load data
-mr <- read_csv("builds/mr.csv", col_types = cols())
+mr <- read_csv("builds/metabolic_rate_data.csv", col_types = cols())
 mam <- read_csv("../PHYLACINE_1.2/Data/Traits/Trait_data.csv", col_types = cols())
 
-# Filter for terrestial
-terrestrial <- mam %>% filter(Terrestrial == 1) %>% pull(Binomial.1.2)
-mr <- mr %>% 
-  filter(Binomial.1.2 %in% terrestrial)
-
 # Summarise sources
-mr %>% count(MR, source)
+mr %>% count(MR.type, Source)
+
+# Summarise species orders families
+mr %>% count(Binomial.1.2)
+mr %>% count(Family.1.2)
+mr %>% count(Order.1.2)
+
+mr %>% 
+  group_by(MR.type) %>% 
+  summarise(N.species = length(unique(Binomial.1.2)), 
+            N.datapoints = n())
 
 # Summarise species info
-mr %>% group_by(MR) %>% 
+mr %>% group_by(MR.type) %>% 
   summarise(n.species = length(unique(Binomial.1.2)),
             n.min = min(table(Binomial.1.2)),
             n.max = max(table(Binomial.1.2)),
@@ -23,9 +31,10 @@ mr %>% group_by(MR) %>%
             log10BM.max = max(log10BM))
 
 mr %>% 
-  group_by(MR) %>% 
+  group_by(MR.type) %>% 
   count(Binomial.1.2) %>%
   count(n) %>% 
-  arrange(MR, n) %>% 
+  arrange(MR.type, n) %>% 
   rename(samples.pr.species = n,
          n.species = nn)
+
